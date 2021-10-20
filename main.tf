4d2ba3e71e8bf107b2b87b09243febdb52df2021
Dave Gillard <dave.gillard@ed.ac.uk>
2021-10-20 14:25:59 +0100

    Switch to terraform to initialise an EC2 instance

--- main.tf
+++ main.tf
@@ -1,35 +1,24 @@
-data "aws_iam_policy_document" "bucket_policy" {
-  statement {
-    sid = "PublicReadGetObject"
-    effect = "Allow"
-    actions = [ "s3:GetObject" ]
-    principals {
-      type = "*"
-      identifiers = [ "*" ]
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
     }
-    resources = [ "arn:aws:s3:::davegthemighty-test.org/*" ]
   }
+
+  required_version = ">= 0.14.9"
 }
 
-/* We can access properties from data sources using this format:
-   ${data.<data_source_type>.<data_source_name>.<property>.
-
-   In this case, we need the JSON document, which the documentation
-   says can be accessed from the .json property. */
-
-resource "aws_s3_bucket" "website" {
-  bucket = "davegthemighty-test.org"  // The name of the bucket.
-  acl    = "public-read"            /* Access control list for the bucket.
-                                       Websites need to be publicly-available
-                                       to the Internet for website hosting to
-                                       work. */
-  policy = "${data.aws_iam_policy_document.bucket_policy.json}"
-  website {
-    index_document = "index.htm"   // The root of the website.
-    error_document = "error.htm"   // The page to show when people hit invalid pages.
-  }
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
 }
 
-output "website_bucket_url" {
-  value = "${aws_s3_bucket.website.website_endpoint}"
+resource "aws_instance" "app_server" {
+  ami           = "ami-0194c3e07668a7e36"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "DavegExampleAppServerInstance"
+  }
 }
